fd7b56292a600106f43613754bd19e8fa6f6671c
AdoyoClifford <adoyoclifford@gmail.com>
2024-04-02 14:05:39 +0300

    initial commit

--- app/src/main/java/com/adoyo/biometrics/MainActivity.kt
+++ app/src/main/java/com/adoyo/biometrics/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.adoyo.biometrics.ui.theme.BiometricsTheme
@@ -22,7 +28,37 @@ class MainActivity : AppCompatActivity() {
         setContent {
             BiometricsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val biometricResult by promptManager.resultPrompt.collectAsState(initial = null)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = {
+                            promptManager.showBiometricPrompt(
+                                title = "Biometric Authentication",
+                                description = "Please authenticate to continue"
+                            )
+                        }) {
+                            Text("Authenticate")
+                        }
+                        biometricResult?.let { result ->
+                            Text(
+                                text = when (result) {
+                                    BiometricManager.BiometricResult.HardwareUnavailable -> "Biometric hardware is unavailable"
+                                    BiometricManager.BiometricResult.FeatureUnavailable -> "Biometric feature is unavailable"
+                                    BiometricManager.BiometricResult.AuthenticationNoteSet -> "Biometric authentication is not set up"
+                                    is BiometricManager.BiometricResult.AuthenticationError -> "Authentication error: ${result.error}"
+                                    BiometricManager.BiometricResult.AuthenticationSucceeded -> "Authentication successful"
+                                    BiometricManager.BiometricResult.AuthenticationCancelled -> TODO()
+                                    BiometricManager.BiometricResult.AuthenticationFailed -> TODO()
+                                }
+                            )
+                        }
 
+                    }
                 }
             }
         }
